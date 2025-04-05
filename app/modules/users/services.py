0dc27cffc0ed4a9255e9f5f94e5be32b2e4fd5d7
bilal mulugeta <bilalmulugeta1610@gmail.com>
2025-04-05 16:21:32 +0300

    refactor(services): modify user create account with argon2 hashing

--- app/modules/users/services.py
+++ app/modules/users/services.py
@@ -1,10 +1,13 @@
 from app.database import session
+from app.core.utils import hash_password
+
 from .models import *
 
-
 def db_create_user_account(user_data:UsersCreate):
+    hashed_password = hash_password(user_data.password)
     with session:
-        db_user = Users.model_validate(user_data)
+        extra_data = {"hashed_password": hashed_password}
+        db_user = Users.model_validate(user_data,update=extra_data)
         session.add(db_user)
         session.commit()
         session.refresh(db_user)

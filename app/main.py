87bff72afa0d7bc9cbdfa8f24a27908cfb6a7bf1
bilal mulugeta <bilalmulugeta1610@gmail.com>
2025-04-05 11:19:31 +0300

    backend: feat(config): add .env configuration

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 
+from app.core.config import setting_dep
+
 app = FastAPI()
 
 
@@ -8,6 +10,10 @@ app = FastAPI()
 @app.get("/healthy")
 def healthy():
     return {"status": "OK"}
+
+@app.get("/info")
+def info(settings: setting_dep):
+    return settings
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=7000, reload=True)

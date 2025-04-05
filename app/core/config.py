1a0b0ac8a495330dd4e49b79eb4afcf3e1d79917
bilal mulugeta <bilalmulugeta1610@gmail.com>
2025-04-05 11:19:31 +0300

    feat(config): add .env configuration

---
+++ app/core/config.py
@@ -0,0 +1,21 @@
+from functools import lru_cache
+from typing import Annotated
+
+from fastapi import Depends
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    database_url: str  # Use SQLite initially
+    secret_key: str  # Generate a secure key
+    algorithm: str
+    access_token_expire_minutes: int
+
+    model_config = SettingsConfigDict(env_file="../.env")
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+setting_dep = Annotated[Settings, Depends(get_settings)]

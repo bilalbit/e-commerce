380cb67f7627cfcc54d63fd6526ba361b760d2e0
bilal mulugeta <bilalmulugeta1610@gmail.com>
2025-04-05 15:31:17 +0300

    backend: feat(utils): add argon2 hashing algorithm - add salt configuration to core config

--- app/core/config.py
+++ app/core/config.py
@@ -10,6 +10,7 @@ class Settings(BaseSettings):
     secret_key: str  # Generate a secure key
     algorithm: str
     access_token_expire_minutes: int
+    salt: str
 
     model_config = SettingsConfigDict(env_file="../.env")
 
---
+++ app/core/utils.py
@@ -0,0 +1,21 @@
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+
+from app.core.config import setting_dep
+
+# Create a PasswordHasher instance
+ph = PasswordHasher()
+#change 256hex value for every project and make sure to do not change it afterward thank u
+
+# Function to hash a password
+def hash_password(psk: str) -> str:
+    psk_with_salt = psk + setting_dep.salt
+    hashed_password = ph.hash(psk_with_salt)
+    return hashed_password
+# Function to verify user password
+def verify_password(hashed_password: str,psk:str)->bool:
+    psk_with_salt = psk + setting_dep.salt
+    try:
+        return ph.verify(hashed_password, psk_with_salt)
+    except VerifyMismatchError:
+        return False

20a3caa622a1e8d1706dd62f1688bae4f6e890a3
bilal mulugeta <bilalmulugeta1610@gmail.com>
2025-04-08 14:46:18 +0300

    refactor(auth): refactor role of user to be added from query fix(auth): bug that create user mode even if dependency doesn't meet

--- app/modules/auth/routes.py
+++ app/modules/auth/routes.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter, HTTPException,status
 
 from app.database import session
-from app.modules.sellers.models import Sellers, SellersCreate
+from app.modules.sellers.models import SellersCreate
 from app.modules.users.models import *
 from app.modules.customers.models import *
 from app.modules.users.services import db_create_user_account
@@ -15,10 +15,12 @@ router = APIRouter(
 @router.post('/register/{role}')
 def register(role: RoleType,user_data:UsersCreate,customer_data:CustomersCreate | None = None,seller_data: SellersCreate | None = None):
     if customer_data is not None and seller_data is not None:
-        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,detail="only one account can be created at a time either as a seller or customer.")
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,detail="only one account can be created at a time😑.either as a seller or customer.")
     with session:
-        db_user = db_create_user_account(user_data)
         if role is RoleType.customer:
+            if customer_data is None:
+                raise HTTPException(status_code=status.HTTP_424_FAILED_DEPENDENCY,detail="customer data is required.")
+            db_user = db_create_user_account(user_data,role)
             db_customer = Customers.model_validate(customer_data)
             db_user.customer = db_customer
             session.add(db_user)
@@ -26,6 +28,9 @@ def register(role: RoleType,user_data:UsersCreate,customer_data:CustomersCreate 
             session.refresh(db_user)
             return db_user
         elif role is RoleType.seller:
+            if seller_data is None:
+                raise HTTPException(status_code=status.HTTP_424_FAILED_DEPENDENCY,detail="seller data is required.")
+            db_user = db_create_user_account(user_data,role)
             db_seller = Sellers.model_validate(seller_data)
             db_user.seller = db_seller
             session.add(db_user)
@@ -33,4 +38,4 @@ def register(role: RoleType,user_data:UsersCreate,customer_data:CustomersCreate 
             session.refresh(db_user)
             return db_user
         else:
-            return db_user
+            return db_create_user_account(user_data,role)

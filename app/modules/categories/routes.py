7d124526ffeecf2292b6efc2ef8441ffd520a523
bilal mulugeta <bilalmulugeta1610@gmail.com>
2025-04-14 08:21:47 +0300

    feat(categories): add categories and route - add category - delete category - update category - get all category and - get category by id

---
+++ app/modules/categories/routes.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter
+
+from .dependencies import current_user_dependency, admin_and_seller_only
+from .services import *
+
+router = APIRouter(
+    prefix="/categories",
+    tags=["categories"],
+)
+
+
+@router.get('/')
+def get_categories():
+    return db_get_categories()
+
+
+@router.get('/{id}')
+def get_categories_by_id(id: uuid.UUID):
+    return db_get_categories_by_id(id)
+
+
+@router.post('/')
+def add_categories(categories: CategoriesCreate, user: current_user_dependency):
+    return db_add_categories(categories)
+
+
+@router.patch('/{id}')
+def update_categories(id: uuid.UUID, categories_data: CategoriesUpdate, user: current_user_dependency):
+    admin_and_seller_only(user)
+    return db_update_categories(id, categories_data)
+
+
+@router.delete('/{id}')
+def delete_categories(id: uuid.UUID,  user: current_user_dependency):
+    admin_and_seller_only(user)
+    return db_delete_categories(id)

730a3ef33007fa123b5389ceb09b98fd87545e8f
bilal mulugeta <bilalmulugeta1610@gmail.com>
2025-04-18 10:46:16 +0300

    backend: feat(wishlist): add cart and route - add wishlist item - delete wishlist item - clear wishlist items - get wishlist items - add wishlist route to api route - add wishlist model to global module model(__init__.py)

--- app/core/routes.py
+++ app/core/routes.py
@@ -6,6 +6,7 @@ from app.modules.sellers.routes import router as sellers_router
 from app.modules.categories.routes import router as categories_router
 from app.modules.products.routes import router as products_router
 from app.modules.carts.routes import router as carts_router
+from app.modules.wishlist.routes import router as wishlist_router
 api_router = APIRouter()
 
 api_router.include_router(auth_router)
@@ -15,3 +16,4 @@ api_router.include_router(sellers_router)
 api_router.include_router(categories_router)
 api_router.include_router(products_router)
 api_router.include_router(carts_router)
+api_router.include_router(wishlist_router)
---
+++ app/modules/wishlist/services.py
@@ -0,0 +1,59 @@
+from fastapi import HTTPException
+from sqlmodel import select, Session
+
+from .models import *
+from app.database import session
+
+
+def db_get_or_create_wishlist(db: Session, customer_id: uuid.UUID):
+    db_wishlist = db.exec(select(Wishlists).where(Wishlists.customer_id == customer_id)).first()
+    if db_wishlist is None:
+        db_wishlist = Wishlists(customer_id=customer_id)
+        db.add(db_wishlist)
+        db.commit()
+        db.refresh(db_wishlist)
+    return db_wishlist
+
+
+def db_get_wishlists(customer_id: uuid.UUID):
+    with session:
+        db_wishlist = db_get_or_create_wishlist(session, customer_id)
+        if db_wishlist.wishlist_items is not None:
+            return db_wishlist
+        raise HTTPException(status_code=404, detail="wishlist Not found")
+
+
+def db_add_wishlist(customer_id: uuid.UUID, product_id: uuid.UUID):
+    with session:
+        db_wishlist = db_get_or_create_wishlist(session, customer_id)
+        db_wishlist_item = Wishlist_Items(product_id=product_id,
+                                          wishlist_id=db_wishlist.id)
+        db_wishlist.wishlist_items.append(db_wishlist_item)
+        session.add(db_wishlist)
+        session.commit()
+        session.refresh(db_wishlist)
+        return db_wishlist
+
+
+def db_delete_wishlist(wishlist_item_id: uuid.UUID, customer_id: uuid.UUID):
+    with session:
+        db_wishlist = db_get_or_create_wishlist(session, customer_id)
+        db_wishlist_item = session.exec(
+            select(Wishlist_Items).where(
+                Wishlist_Items.id == wishlist_item_id,
+                Wishlist_Items.wishlist_id == db_wishlist.id
+            )
+        ).one()
+        if db_wishlist_item is None:
+            raise HTTPException(status_code=404, detail="wishlist item  not found")
+        print(db_wishlist_item)
+        session.delete(db_wishlist_item)
+        session.commit()
+
+
+def db_clear_wishlist(customer_id: uuid.UUID):
+    with session:
+        db_wishlist = session.exec(select(Wishlists).where(Wishlists.customer_id == customer_id)).first()
+        if db_wishlist:
+            session.delete(db_wishlist)
+            session.commit()

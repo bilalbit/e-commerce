17bd7eea34f96c3448386d8afd32d8fb93fa6aed
bilal mulugeta <bilalmulugeta1610@gmail.com>
2025-04-20 21:20:01 +0300

    backend: refactor(wishlist dependencies): get authorization from global dependencies feat(wishlist service): add status of corresponding methods - add wishlist to check if the wishlist item already exist

--- app/modules/wishlist/services.py
+++ app/modules/wishlist/services.py
@@ -1,4 +1,4 @@
-from fastapi import HTTPException
+from fastapi import HTTPException,status
 from sqlmodel import select, Session
 
 from .models import *
@@ -18,14 +18,23 @@ def db_get_or_create_wishlist(db: Session, customer_id: uuid.UUID):
 def db_get_wishlists(customer_id: uuid.UUID):
     with session:
         db_wishlist = db_get_or_create_wishlist(session, customer_id)
-        if db_wishlist.wishlist_items is not None:
+        if db_wishlist.wishlist_items:
             return db_wishlist
-        raise HTTPException(status_code=404, detail="wishlist Not found")
+        raise HTTPException(status_code=404, detail="wishlist items is Not found")
 
 
 def db_add_wishlist(customer_id: uuid.UUID, product_id: uuid.UUID):
     with session:
         db_wishlist = db_get_or_create_wishlist(session, customer_id)
+        is_product_added = session.exec(
+            select(Wishlist_Items).where(
+                Wishlist_Items.product_id == product_id,
+                Wishlist_Items.wishlist_id == db_wishlist.id
+            )
+        ).first()
+        if is_product_added:
+            raise HTTPException(status_code=status.HTTP_412_PRECONDITION_FAILED, detail="product already added")
+
         db_wishlist_item = Wishlist_Items(product_id=product_id,
                                           wishlist_id=db_wishlist.id)
         db_wishlist.wishlist_items.append(db_wishlist_item)
@@ -43,10 +52,9 @@ def db_delete_wishlist(wishlist_item_id: uuid.UUID, customer_id: uuid.UUID):
                 Wishlist_Items.id == wishlist_item_id,
                 Wishlist_Items.wishlist_id == db_wishlist.id
             )
-        ).one()
+        ).first()
         if db_wishlist_item is None:
-            raise HTTPException(status_code=404, detail="wishlist item  not found")
-        print(db_wishlist_item)
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="wishlist item  not found")
         session.delete(db_wishlist_item)
         session.commit()
 

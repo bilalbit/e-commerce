35aa1ab8aa7e23723e1e204681c97694f56bf643
bilal mulugeta <bilalmulugeta1610@gmail.com>
2025-04-20 20:30:36 +0300

    backend: refactor(products dependencies): get authorization from global dependencies

--- app/modules/products/models.py
+++ app/modules/products/models.py
@@ -7,17 +7,25 @@ from sqlmodel import Field, SQLModel, Relationship
 from app.core.models import TimeStampMixin
 
 if TYPE_CHECKING:
-    from app.modules import Categories,Reviews
+    from app.modules import Categories, Reviews
 
 
-class ProductsBase(TimeStampMixin):
+class ProductsBase(SQLModel):
     name: str = Field(min_length=3, index=True)
     description: str = Field(max_length=500)
-    price: Decimal = Field(decimal_places=2, index=True)
+    price: Decimal = Field(decimal_places=2, index=True, ge=1)
     stock_quantity: int = Field(default=1, ge=1)
     is_available: bool = Field(default=True)
-    category_id: uuid.UUID | None = Field(default=None, foreign_key="categories.id",
-                                          index=True, ondelete="SET NULL", nullable=True)
+    category_id: uuid.UUID = Field(default=None, foreign_key="categories.id",
+                                   index=True, ondelete="SET NULL", nullable=True)
+
+
+class Products(ProductsBase, TimeStampMixin, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
+    seller_id: uuid.UUID | None = Field(foreign_key="sellers.id", ondelete="CASCADE")
+    ##Relationships
+    category: "Categories" = Relationship(back_populates="products")
+    reviews: list["Reviews"] = Relationship(back_populates="products")
 
 
 class ProductsCreate(ProductsBase):
@@ -33,9 +41,5 @@ class ProductsUpdate(SQLModel):
     category_id: uuid.UUID | None = None
 
 
-class Products(ProductsBase, table=True):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, index=True)
-    seller_id: uuid.UUID | None = Field(foreign_key="sellers.id", ondelete="CASCADE")
-    ##Relationships
-    category: "Categories" = Relationship(back_populates="products")
-    reviews: list["Reviews"] = Relationship(back_populates="products")
+class ProductsPublic(ProductsBase):
+    id: uuid.UUID

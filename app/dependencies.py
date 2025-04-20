c4b9e928255900c3fc45d20ee9811af3b0cdc248
bilal mulugeta <bilalmulugeta1610@gmail.com>
2025-04-20 20:29:58 +0300

    fix(dependencies): fix fastapi.exceptions.FastAPIError: Invalid args for response error

--- app/dependencies.py
+++ app/dependencies.py
@@ -6,7 +6,7 @@ from sqlmodel import Session, select
 
 from app.core.security import current_user_dependency
 from app.database import session
-from app.modules import Customers, Sellers
+from app.modules import Customers, Sellers,Users
 
 current_user_context: ContextVar[dict | None] = ContextVar("current_user_context", default=None)
 
@@ -15,9 +15,9 @@ def get_current_user_data():
     return current_user_context.get()
 
 
-def get_current_customer(user: current_user_dependency,db_session:Session = session):
+def get_current_customer(user: current_user_dependency, db_session: Session = Depends(lambda: session)):
     if user["role"] != "customer":
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Customer role required")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Customer role required")
     with db_session:
         customer = db_session.exec(
             select(Customers).where(
@@ -29,23 +29,21 @@ def get_current_customer(user: current_user_dependency,db_session:Session = sess
         return customer
 
 
-def get_current_seller(user: current_user_dependency,db_session:Session = session):
+def get_current_seller(user: current_user_dependency, db_session: Session = Depends(lambda: session)):
     if user["role"] != "seller":
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Seller role required")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Seller role required")
     with db_session:
         seller = db_session.exec(
-            select(Sellers).where(
-                Sellers.user_id == user["id"]
-            )
+            select(Sellers).where(Sellers.user_id == user["id"])
         ).first()
         if not seller:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Seller not found")
         return seller
 
 
-def get_current_admin(user: current_user_dependency,db_session:Session = session):
+def get_current_admin(user: current_user_dependency,db_session: Session = Depends(lambda: session)):
     if user["role"] != "admin":
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Admin role required")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Admin role required")
     with db_session:
         admin = db_session.exec(
             select(Sellers).where(
@@ -57,9 +55,9 @@ def get_current_admin(user: current_user_dependency,db_session:Session = session
         return admin
 
 
-current_customer_dependency = Annotated[dict, Depends(get_current_customer)]
-current_seller_dependency = Annotated[dict, Depends(get_current_seller)]
-current_admin_dependency = Annotated[dict, Depends(get_current_admin)]
+current_customer_dependency = Annotated[Customers, Depends(get_current_customer)]
+current_seller_dependency = Annotated[Sellers, Depends(get_current_seller)]
+current_admin_dependency = Annotated[Users, Depends(get_current_admin)]
 
 
 def admin_and_customer_only(user: current_user_dependency):
@@ -78,10 +76,21 @@ def admin_and_seller_only(user: current_user_dependency):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Only admin and seller authorized to do this operation"
         )
+
+
 def admin_only(user: current_user_dependency):
     user_role = user["role"]
     if user_role != "admin":
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Only admin authorized to do this operation"
+        )
+
+
+def seller_only(user: current_user_dependency):
+    user_role = user["role"]
+    if  user_role != "seller":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Only seller authorized to do this operation"
         )

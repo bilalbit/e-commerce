0e1e8bbeb5347c26ef4e556710e9a9e76bab81f4
bilal mulugeta <bilalmulugeta1610@gmail.com>
2025-04-20 22:38:29 +0300

    backend: refactor(orders dependencies): get authorization from global dependencies feat(orders): add status of corresponding methods to services - add order by product
    fix(cart): bug on get cart by id method database session
    
    feat(product): update product stock quantity

--- app/modules/order/services.py
+++ app/modules/order/services.py
@@ -1,26 +1,26 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 from sqlmodel import select, Session
 
 from app.core.services import get_record_by_id
 from app.database import session
 from app.modules import Products
 from app.modules.carts.services import db_get_cart, db_clear_cart
-from app.modules.products.services import db_update_product
+from app.modules.products.services import db_update_product_stock_quantity, db_get_products_by_id
 from .models import *
-from ..products.models import ProductsUpdate
 
 
-def db_get_order_by_id(order_id: uuid.UUID,customer_id: uuid.UUID,db_session: Session = session):
+def db_get_order_of_customer_by_id(order_id: uuid.UUID, customer_id: uuid.UUID, db_session: Session = session):
     with db_session:
         db_order = db_session.exec(
             select(Orders).where(
                 Orders.id == order_id,
                 Orders.customer_id == customer_id,
             )
-        ).one()
+        ).first()
         if db_order is not None and db_order.order_items:
             return db_order
-        raise HTTPException(status_code=404,detail="not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="order not found")
+
 
 def db_get_orders(customer_id: uuid.UUID):
     with session:
@@ -31,25 +31,25 @@ def db_get_orders(customer_id: uuid.UUID):
         ).all()
         return db_orders
 
+
 def db_create_order(shipping_address: str, customer_id: uuid.UUID):
     with session:
-        db_cart = db_get_cart(customer_id)
+        db_cart = db_get_cart(customer_id, session)
         db_cart_items = db_cart.cart_items
         total_amount = Decimal("0.00")
-        order_items_data:list["OrderItem"] = []
+        order_items_data: list["OrderItem"] = []
         for item in db_cart_items:
-            db_product = get_record_by_id(item.product_id, Products,"products")
+            db_product = get_record_by_id(item.product_id, Products, "products")
             price_at_purchase = Decimal(str(db_product.price))
             total_amount += price_at_purchase * item.quantity
-            db_update_product(db_product.id,ProductsUpdate(stock_quantity = db_product.stock_quantity - item.quantity))
+            db_update_product_stock_quantity(db_product.id, db_product.stock_quantity - item.quantity)
             order_items_data.append(
                 OrderItem(
-                    product_id= db_product.id,
-                    quantity = item.quantity,
-                    price_at_purchase= price_at_purchase
+                    product_id=db_product.id,
+                    quantity=item.quantity,
+                    price_at_purchase=price_at_purchase
                 )
             )
-
 
         db_order = Orders(
             customer_id=customer_id,
@@ -63,11 +63,38 @@ def db_create_order(shipping_address: str, customer_id: uuid.UUID):
         session.refresh(db_order)
         db_clear_cart(customer_id)
 
-        return get_record_by_id(db_order_id,Orders,"Order")
-def db_update_order_status(id: uuid.UUID,customer_id: uuid.UUID,status: OrderStatus):
+        return db_get_order_of_customer_by_id(db_order_id, customer_id)
+
+
+def db_add_product_to_order(product_id: uuid.UUID, order_create: OrderCreate, customer_id: uuid.UUID):
     with session:
-        db_order = db_get_order_by_id(id,customer_id,session)
-        db_order.status = status
+        db_product = db_get_products_by_id(product_id)
+        db_update_product_stock_quantity(db_product.id, order_create.quantity)
+        price_at_purchase = Decimal(db_product.price)
+        total_amount = price_at_purchase * order_create.quantity
+        db_order_item = OrderItem(
+            product_id=db_product.id,
+            quantity=order_create.quantity,
+            price_at_purchase=price_at_purchase
+        )
+        db_order = Orders(
+            customer_id=customer_id,
+            shipping_address=order_create.shipping_address,
+            total_amount=total_amount
+        )
+        db_order.order_items.append(db_order_item)
+        session.add(db_order)
+        session.commit()
+        session.refresh(db_order)
+        return db_get_order_of_customer_by_id(db_order.id, customer_id)
+
+
+def db_update_order_status(id: uuid.UUID, order_status: OrderStatus):
+    with session:
+        db_order = session.get(Orders, id)
+        if db_order is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found")
+        db_order.status = order_status
         session.add(db_order)
         session.commit()
         session.refresh(db_order)
